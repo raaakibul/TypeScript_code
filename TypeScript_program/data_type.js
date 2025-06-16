2923aea444d261fc06a0dac1acdebd878c54bfab
rakibul-islam <raakibul.uap@gmail.com>
2025-06-17 00:15:30 +0600

    typescript datatype

--- TypeScript_program/data_type.js
+++ TypeScript_program/data_type.js
@@ -6,3 +6,15 @@ var tuple = ["Rakibul", 25];
 var anything = "Could be anything";
 console.log(isDone);
 console.log(age, userName, numbers, tuple, anything);
+console.log("boolean: ".concat(isDone, ", age:").concat(age, ", Username: ").concat(userName, ", numbers: ").concat(numbers, ", tuple:").concat(tuple, ", anything: ").concat(anything));
+// union type
+var userId;
+userId = "101";
+userId = 101;
+userId = true;
+console.log("UserId:".concat(userId));
+function display(userId) {
+    console.log(userId);
+}
+display("101");
+display(101);
--- TypeScript_program/data_type.ts
+++ TypeScript_program/data_type.ts
@@ -4,5 +4,21 @@ let userName: string = "Rakibul";
 let numbers: number[] = [1, 2, 3, 4];
 let tuple: [string, number] = ["Rakibul", 25];
 let anything: any = "Could be anything";
+
 console.log(isDone);
 console.log(age,userName,numbers,tuple,anything);
+
+console.log(`boolean: ${isDone}, age:${age}, Username: ${userName}, numbers: ${numbers}, tuple:${tuple}, anything: ${anything}`);
+
+// union type
+let userId: string | number | boolean;
+userId = "101";
+userId = 101;
+userId = true;
+console.log(`UserId:${userId}`);
+
+function display(userId:string|number){
+    console.log(userId);
+}
+display("101");
+display(101);
