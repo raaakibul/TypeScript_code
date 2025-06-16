e9b3223162a6e41767e0acc93b96e0fb945464b6
rakibul-islam <raakibul.uap@gmail.com>
2025-06-17 00:21:59 +0600

    array

---
+++ TypeScript_program/array.js
@@ -0,0 +1,8 @@
+// let userNames: string[]; 
+var userNames;
+userNames = ["A", "B", "C", "D"];
+console.log(userNames);
+console.log(userNames[0]);
+var multipleTypes;
+multipleTypes = [1, "name"];
+console.log(multipleTypes);
---
+++ TypeScript_program/array.ts
@@ -0,0 +1,9 @@
+// let userNames: string[]; 
+let userNames:Array<string>;
+userNames = ["A","B","C","D"]
+console.log(userNames);
+console.log(userNames[0]);
+
+let multipleTypes: (string | number)[];
+multipleTypes = [1,"name"]
+console.log(multipleTypes);
